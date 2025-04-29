af9fc2dd2de0a92c962fcbfe4a8f1b4825c8916a
ahmad.addakhakhny <ahmed.addakhakhny@gmail.com>
2025-04-29 22:22:30 +0300

    Log Project Info 	- Set CXX standards to be 17 	- Disable CXX Compiler extensions 	- Set template file to keep track of the project_name and Project_version

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,12 @@
 #include "printHello.hpp"
+#include "config.hpp"
 
 
 int main() {
   print printObject;
 
+  std::cout << project_name << " Project Started\n";
+  std::cout << "Project Version is: " << project_version << std::endl;
   printObject.printHelloWorld();
   
   return 0;
