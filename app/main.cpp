63eca4414eb9ebd8eb5476238fe1e28e7b157d29
ahmad.addakhakhny <ahmed.addakhakhny@gmail.com>
2025-05-01 23:35:45 +0300

    Third Party Library 	- Add CPM as external package manager to fetch CMake projects from GitHup 	- Add an external module to serialize/deserialize json objects

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,6 @@
 #include "printHello.hpp"
 #include "config.hpp"
-
+#include <nlohmann/json.hpp>
 
 int main() {
   print printObject;
@@ -8,6 +8,11 @@ int main() {
   std::cout << project_name << " Project Started\n";
   std::cout << "Project Version is: " << project_version << std::endl;
   std::cout << "project git commit sha: " << app_git_commit_sha << std::endl;
+  std::cout << "Nlohmann_json project Version: v" 
+  << NLOHMANN_JSON_VERSION_MAJOR << "."
+  << NLOHMANN_JSON_VERSION_MINOR << "."
+  << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+  
 
   #if DEBUG_LOG
   std::cout << "#### Print DEBUG_LOG Message" << std::endl;
