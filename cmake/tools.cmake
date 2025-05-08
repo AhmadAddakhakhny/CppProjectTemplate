28a86bb1c849ff235a276e13b1297694aa5d6908
ahmad.addakhakhny <ahmed.addakhakhny@gmail.com>
2025-05-08 12:53:47 +0300

    Utilize Clang-format 	- Write a cmake function to regex over project source directories to format all of the files via clang-format
    [feat/bug/test/other] #id

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -34,3 +34,38 @@ function(add_clang_tidy_to_target target)
         message(WARNING "CLANGTIDY NOT FOUND")
     endif()
 endfunction()
+                                                                    
+function(add_clang_format_target) 
+  if (NOT ${ENABLE_CLANG_FORMAT}) 
+    return () 
+  endif()
+  
+  find_package(Python3 COMPONENTS Interpreter) 
+  
+  if (NOT ${Python_FOUND})
+    return() 
+  endif() 
+  
+  file(GLOB_RECURSE CMAKE_FILES_CC "*/*.cc")
+  file(GLOB_RECURSE CMAKE_FILES_CPP "*/*.cpp") 
+  file(GLOB_RECURSE CMAKE_FILES_H "*/*.h") 
+  file(GLOB_RECURSE CMAKE_FILES_HPP "*/*.hpp") 
+  set(CPP_FILES ${CMAKE_FILES_CC} ${CMAKE_FILES_CPP} ${CMAKE_FILES_H} ${CMAKE_FILES_HPP})
+  list(FILTER 
+  CPP_FILES
+  EXCLUDE
+  REGEX 
+  "${CMAKE_SOURCE_DIR}/(build|external)/.*")
+
+  find_program(CLANGFORMAT clang-format) 
+
+  if (CLANGFORMAT)
+    message(STATUS "Added Clang Format")
+    string(REPLACE ";" " " CPP_FILES_STR "${CPP_FILES}")
+    set(DUMMY "") 
+    set(RUN_CMD "python3 ${CMAKE_SOURCE_DIR}/tools/run-clang-format.py ${CPP_FILES_STR} --in-place") 
+    execBashCommand(${RUN_CMD} DUMMY)
+  else()
+    message(WARNING "CLANG FORMAT NOT FOUND")
+  endif()
+endfunction()
